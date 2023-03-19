024571b24e21c593d802927c59801cd819fcd0c2
Дубовицкий Дмитрий Геннадьевич <DDubovitskiy@IBS.RU>
2023-03-19 23:49:20 +0300

    OTUS-HW1 Adjust methods

--- python-engine/sql_app/main.py
+++ python-engine/sql_app/main.py
@@ -18,12 +18,14 @@ def get_db():
         db.close()
 
 
-@app.post("/users/", response_model=schemas.User)
+@app.post("/user/register", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db, user=user)
+    result = crud.create_user(db=db, user=user)
+    # Optionally try to catch 400, but better 422
+    return result
 
 
 @app.get("/users/", response_model=list[schemas.User])
@@ -32,10 +34,11 @@ def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return users
 
 
-@app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
+@app.get("/user/get/{id}", response_model=schemas.User)
+def read_user(id: int, db: Session = Depends(get_db)):
+    #400, но 422 parser срабатывает раньше
+    db_user = crud.get_user(db, user_id=id)
     if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=404, detail="Анкета не найдена")
     return db_user
 
